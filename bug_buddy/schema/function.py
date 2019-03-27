7489d01b784fb0b8c7f2d8ccc77805ec2071b813
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-03-26 22:44:36 -0700

    "bug_buddy_commit"

--- bug_buddy/schema/function.py
+++ bug_buddy/schema/function.py
@@ -45,33 +45,13 @@ class Function(Base):
     def __init__(self,
                  repository,  # Repository - need to figure out typing for
                               # in cases where they both refer to each other
-                 node: ast.AST,
                  file_path: str):
         '''
         Creates a new Function instance.
         '''
         self.repository = repository
-        self.node = node
         self.name = node.name
         self.file_path = file_path
-
-    @property
-    def ast_node(self):
-        '''
-        Returns the AST node representation of the function.  If it is not
-        already loaded, it will retrieve it from the source file
-        '''
-        if not self.loaded_node():
-            msg = 'you need to load the ast node for {}'.format(self.name)
-            raise Exception(msg)
-
-        return self.node
-
-    def loaded_node(self):
-        '''
-        Returns whether or not the AST node for the function has been loaded
-        '''
-        return hasattr(self, 'node')
 
     @property
     def latest_history(self):
@@ -86,65 +66,6 @@ class Function(Base):
         Returns the absolute path
         '''
         return os.path.join(self.repository.path, self.file_path)
-
-    def remove_line(self, line):
-        '''
-        Removes a particular line from the function
-        '''
-        with open(self.abs_path, 'r') as f:
-            contents = f.readlines()
-
-        content = contents.pop(line - 1)
-        logger.info('Removed line: "{}" from {}'
-                    .format(content.strip(), self.file_path))
-
-        with open(self.abs_path, 'w') as f:
-            f.writelines(contents)
-
-    def prepend_statement(self, statement, offset: int=0):
-        '''
-        Writes a statement to the beginning of the function
-        '''
-        def _is_comment(node):
-            '''
-            Checks to see if the node is a comment.  We need to because we do
-            not want to add our statement into the comment.  For some reason,
-            comments lineno is the last part of the comment.
-            '''
-            return (True if hasattr(node, 'value') and
-                    isinstance(node.value, ast.Str) else False)
-
-        # Get the first node in the function, which is it's first statement.
-        # We will add the statement here
-        first_node = self.ast_node.body[0]
-        first_line_in_function = first_node.lineno
-
-        # scoot down one function if the first node is a comment
-        first_line_in_function += 1 if _is_comment(first_node) else 0
-        first_line_in_function += offset
-
-        # note that a comment after the function does not seem to have a
-        # column offset, and instead returns -1.
-        column_offset = (first_node.col_offset if first_node.col_offset != -1
-                         else self.ast_node.col_offset + 4)
-        indentation = ' ' * column_offset
-        indented_statement = indentation + statement + '\n'
-
-        with open(self.abs_path, 'r') as f:
-            contents = f.readlines()
-
-        contents.insert(first_line_in_function - 1, indented_statement)
-
-        with open(self.abs_path, 'w') as f:
-            f.writelines(contents)
-
-        logger.info('Added "{statement}" to {file} | {function_name}@{lineno}'
-                    .format(statement=statement,
-                            file=self.file_path,
-                            function_name=self.ast_node.name,
-                            lineno=first_line_in_function))
-
-        return first_line_in_function
 
     def __repr__(self):
         '''
