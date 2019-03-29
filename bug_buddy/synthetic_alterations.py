7d94a182587666209178522e8e93eadc48a59dc7
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-03-28 20:48:20 -0700

    "bug_buddy_commit"

--- bug_buddy/synthetic_alterations.py
+++ bug_buddy/synthetic_alterations.py
@@ -230,6 +230,7 @@ def apply_synthetic_diff(commit: Commit, diff_subset: DiffList):
         msg = ('More than one diff created in the apply_synthetic_diff step. '
                'The diffs are: {}'.format(new_diffs))
         assert len(new_diffs) == 1, msg
+
         new_diff = save_diffs(commit.repository, commit, new_diffs)[0]
 
 
