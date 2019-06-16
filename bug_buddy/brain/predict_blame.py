50efc870d345b355f486f88e3e5ea957d6f89a10
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-06-16 00:12:27 -0700

    Experiment_2_in_progress

--- bug_buddy/brain/predict_blame.py
+++ bug_buddy/brain/predict_blame.py
@@ -21,16 +21,23 @@ from bug_buddy.logger import logger
 from bug_buddy.schema import Commit, Repository, TestResult
 
 import tensorflow as tf
-
+# importing Keras from Tensorflow is necessary for transforming a Keras
+# model into an Estimator
+import tensorflow as tensorflow
+tensorflow.enable_eager_execution()
+from tensorflow.train import AdamOptimizer
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.layers import Dense, Activation
 
 numpy.set_printoptions(suppress=True, precision=4)
-BLAME_PREDICTION_MODEL = 'predict_blame.h5'
+BLAME_PREDICTION_MODEL = 'predict_blame_{attempt_id}.h5'
 
 test_result_prediction_model = None
 
 # A TfRecord file that stores the vectors.  Used for quick ingestion during
 # training and caches the computed output of each test failure.
-TF_RECORD_FILE = get_output_dir('train.tfrecords')
+TF_RECORD_FILE = get_output_dir('exp_2_predict_blame.tfrecords')
 
 
 def to_tf_float(value):
@@ -67,10 +74,12 @@ def write_records(repository: Repository):
     writer.close()
 
 
-def commit_generator(repository_id: int, batch_size: int):
+def commit_generator(repository_id: int, batch_size: int, no_noise_epochs=200):
     '''
     Returns augmented commit data for training
     '''
+    epoch_num = 1
+    add_noise = False
     with session_manager() as session:
         repository = get(session, Repository, id=repository_id)
         while True:
@@ -86,12 +95,18 @@ def commit_generator(repository_id: int, batch_size: int):
                     continue
 
                 print(commit)
-                features.extend(commit_to_features(commit, add_noise=True))
+
+                add_noise = False
+                # We should only start adding noise after 500 epochs
+                if epoch_num > no_noise_epochs:
+                    add_noise = True
+
+                features.extend(commit_to_features(commit, add_noise=add_noise))
                 labels.extend(commit_to_labels(commit))
 
             print('Yielding batch size of {}'.format(batch_size))
-            print('Features size {}'.format(len(features)))
-            print('Labels size {}'.format(len(labels)))
+            epoch_num += 1
+
             yield numpy.stack(features), numpy.stack(labels)
 
 
@@ -140,37 +155,50 @@ def train(repository: Repository):
     Creates and trains a neural network.  It then exports a model.
     '''
     # bringing this into the method because keras takes so long to load
+    model = get_attempt_2_model()
 
-    # importing Keras from Tensorflow is necessary for transforming a Keras
-    # model into an Estimator
-    import tensorflow as tensorflow
-    tensorflow.enable_eager_execution()
-    from tensorflow.train import AdamOptimizer
-    from tensorflow.python.keras.models import Sequential
-    from tensorflow.python.keras.callbacks import ModelCheckpoint
-    from tensorflow.python.keras.layers import Dense, Activation
+    train_model_keras(repository, model, 2)
 
-    model = Sequential()
-    model.add(Dense(1187, input_shape=(1187, )))
-    model.add(Activation('relu'))
-    model.add(Dense(674))
-    model.add(Activation('relu'))
-    model.add(Dense(337))
-    model.add(Activation('sigmoid'))
 
-    optimizer = AdamOptimizer()
-    # Compile model
-    model.compile(
-        loss='categorical_crossentropy',
-        optimizer=optimizer,
-        metrics=['accuracy'])
+def train_model_keras(repository, model, attempt_id):
+    '''
+    Convert our keras model into a Tensorflow Estimator so that we can easily
+    import TfRecords
+    '''
+    batch_size = 1
 
-    print(model.summary())
+    generator = commit_generator(repository.id, batch_size)
 
-    # Convert our keras model into a Tensorflow Estimator so that we can easily
-    # import TfRecords
-    # estimator = tf.keras.estimator.model_to_estimator(
-    #     keras_model=model, model_dir=get_output_dir())
+    model.save(get_output_dir(BLAME_PREDICTION_MODEL.format(
+        attempt_id=attempt_id)))
+
+    checkpoint_weights_filename = get_output_dir(
+        'predict_blame_{attempt_id}'.format(attempt_id=attempt_id) +
+        '{epoch}.h5f')
+
+    callbacks = [
+        ModelCheckpoint(checkpoint_weights_filename, period=10),
+    ]
+
+    # Fit the model
+    model.fit_generator(
+        generator,
+        epochs=4000,
+        callbacks=callbacks,
+        steps_per_epoch=1,
+        verbose=1)
+
+    model.save(get_output_dir(BLAME_PREDICTION_MODEL.format(
+        attempt_id=attempt_id)))
+
+
+def train_model_as_estimator(model):
+    '''
+    Convert our keras model into a Tensorflow Estimator so that we can easily
+    import TfRecords
+    '''
+    estimator = tf.keras.estimator.model_to_estimator(
+        keras_model=model, model_dir=get_output_dir())
 
     # train_spec = tf.estimator.TrainSpec(
     #     input_fn=lambda: tensor_feeder(
@@ -185,27 +213,71 @@ def train(repository: Repository):
     #         perform_shuffle=False,
     #         batch_size=1))
 
-    # estimator.train(input_fn=tensor_feeder(TF_RECORD_FILE))
+    estimator.train(input_fn=tensor_feeder(TF_RECORD_FILE))
 
-    batch_size = 1
 
-    generator = commit_generator(repository.id, batch_size)
+# def get_attempt_1_model():
+#     '''
+#     Attempt had a flat 1D input made up of two conatenated vectors:
+#         - First part of the vector was the method data in relation to the test
+#           in question.
+#         - Second part of the vector was if the test:
+#             0 - didn't fail
+#             1 - failed
+#             2 - failed and this is the test in question
 
-    checkpoint_weights_filename = get_output_dir('predict_blame_{epoch}.h5f')
+#     Had an average accuracy of 0.159
+#     '''
+#     model = Sequential()
+#     model.add(Dense(1187, input_shape=(1187, )))
+#     model.add(Activation('relu'))
+#     model.add(Dense(674))
+#     model.add(Activation('relu'))
+#     model.add(Dense(337))
+#     model.add(Activation('sigmoid'))
 
-    callbacks = [
-        ModelCheckpoint(checkpoint_weights_filename, period=10),
-    ]
+#     optimizer = AdamOptimizer()
+#     # Compile model
+#     model.compile(
+#         loss='categorical_crossentropy',
+#         optimizer=optimizer,
+#         metrics=['accuracy'])
 
-    # Fit the model
-    model.fit_generator(
-        generator,
-        epochs=4000,
-        callbacks=callbacks,
-        steps_per_epoch=1,
-        verbose=1)
+#     print(model.summary())
+#     return model
 
-    model.save(get_output_dir(BLAME_PREDICTION_MODEL))
+
+def get_attempt_2_model():
+    '''
+    Attempt had a flat 1D input made up of two conatenated vectors:
+        - First part of the vector was the method data in relation to the test
+          in question.
+            - function was altered: 0 or 1
+            - number of times the function has been synthetically blamed: int
+
+        - Second part of the vector was for each test it had two components:
+            - did fail: 0 or 1
+            - is test in question: 0 or 1
+
+    Had an average accuracy of ...
+    '''
+    model = Sequential()
+    model.add(Dense(1700, input_shape=(1700, )))
+    model.add(Activation('relu'))
+    model.add(Dense(674))
+    model.add(Activation('relu'))
+    model.add(Dense(337))
+    model.add(Activation('sigmoid'))
+
+    optimizer = AdamOptimizer()
+    # Compile model
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=optimizer,
+        metrics=['accuracy'])
+
+    print(model.summary())
+    return model
 
 
 def predict_blames(commit: Commit):
@@ -222,7 +294,8 @@ def predict_blame(test_failure: TestResult):
     Predicts using the model
     '''
     from keras.models import load_model
-    model = load_model(get_output_dir(BLAME_PREDICTION_MODEL))
+    model = load_model(get_output_dir(BLAME_PREDICTION_MODEL.format(
+        attempt_id=2)))
     feature = test_failure_to_feature(test_failure)
     prediction_vector = model.predict(numpy.array([feature, ]))
     test_failure.blamed_function_prediction_vector = (
@@ -243,7 +316,8 @@ def validate(repository):
     Validates a model against a repository
     '''
     from keras.models import load_model
-    model = load_model(get_output_dir(BLAME_PREDICTION_MODEL))
+    model = load_model(get_output_dir(BLAME_PREDICTION_MODEL.format(
+        attempt_id=2)))
     commits, validation_features, validation_labels = get_validation_data(repository)
     scores = model.evaluate(validation_features, validation_labels)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
@@ -314,19 +388,29 @@ def test_failure_to_feature(test_failure: TestResult, add_noise=False):
     '''
     Convert a test failure into a feature.  This contains all the features
     for each method in relation to the test
+
+    @returns: a 1D vector made up of two conatenated 1D vectors:
+        - First part of the vector was the method data in relation to the test
+          in question.
+            - function was altered: 0 or 1
+            - number of times the function has been synthetically blamed: int
+
+        - Second part of the vector was for each test it had two components:
+            - did fail: 0 or 1
+            - is test in question: 0 or 1
     '''
-    # get a vector of all the tests that are failing and signify with 2 which
-    # test we are focused on
     test_result_vector = numpy.array([])
+
     for test_result in test_failure.test_run.test_results_ordered:
+        # test is the test in question: 0 or 1
         if test_result.id == test_failure.id:
-            # we are signifying that this is current test that we are concerned
-            # about by setting it to 2.
-            test_result_vector = numpy.append(test_result_vector, 2.0)
-
-        elif test_result.failed:
             test_result_vector = numpy.append(test_result_vector, 1.0)
+        else:
+            test_result_vector = numpy.append(test_result_vector, 0.0)
 
+        #  test failed: 0 or 1
+        if test_result.failed:
+            test_result_vector = numpy.append(test_result_vector, 1.0)
         else:
             test_result_vector = numpy.append(test_result_vector, 0.0)
 
